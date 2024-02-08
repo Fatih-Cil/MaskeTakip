19eed2d2d8bc9783f05f49f12e35aafdd5bbe15a
Fatih <85998424+Fatih-Cil@users.noreply.github.com>
2024-02-08 13:26:37 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string mesaj = "Selam";
+double tutar = 100000;
+int sayi = 100;
+bool girisYaptimi=false;
+
+string ad = "Fatih";
+string soyad = "Çil";
+int dogumYili = 1979;
+long tcNo = 12345678901;
+
+Console.WriteLine(mesaj);
+Console.WriteLine(mesaj);
+
