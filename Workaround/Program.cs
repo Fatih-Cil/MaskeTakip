19dc741053d881cc2016d3d6942d94623e10767b
Fatih <85998424+Fatih-Cil@users.noreply.github.com>
2024-02-08 13:50:05 +0300

    Class create

--- Workaround/Program.cs
+++ Workaround/Program.cs
@@ -1,15 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string mesaj = "Selam";
-double tutar = 100000;
-int sayi = 100;
-bool girisYaptimi=false;
+using System.Runtime.CompilerServices;
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Degiskenler();
+            Vatandas vatandas1 = new Vatandas();
+            
+        }
 
-string ad = "Fatih";
-string soyad = "Çil";
-int dogumYili = 1979;
-long tcNo = 12345678901;
+        static void Degiskenler()
+        {
+            string mesaj = "Selam";
+            double tutar = 100000;
+            int sayi = 100;
+            bool girisYaptimi = false;
 
-Console.WriteLine(mesaj);
-Console.WriteLine(mesaj);
+            string ad = "Fatih";
+            string soyad = "Çil";
+            int dogumYili = 1979;
+            long tcNo = 12345678901;
+
+            Console.WriteLine(mesaj);
+            Console.WriteLine(sayi);
+        }
+
+    }
+
+    public class Vatandas
+    {
+
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+
+}
 
